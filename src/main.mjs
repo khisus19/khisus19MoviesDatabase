d8bfad57b5d22e7298a70ef3e445db19201b9b7b
Jesús Álvarez <khisus19@gmail.com>
2022-07-07 22:06:56 -0500

    Using axios

--- src/main.mjs
+++ src/main.mjs
@@ -1,8 +1,18 @@
 import { API_KEY } from "./api_key.mjs";
+const api = axios.create({
+    baseURL: "https://api.themoviedb.org/3/",
+    headers: {
+        "Content-Type": "application/json;charset=utf-8",
+    },
+    params: {
+        "api_key": API_KEY,
+    }
+})
 
 const btnPrev = document.getElementById("btnPrev");
 const btnNext = document.getElementById("btnNext");
 let currentPage = 1;
+
 btnNext.addEventListener("click", () => {
     if (currentPage < 1000) {
         currentPage++;
@@ -17,30 +27,27 @@ btnPrev.addEventListener("click", () => {
 })
 
 const loadMovies = async() => {
+    const trendingUrl = `trending/movie/week?page=${currentPage}`;
+    const topRatedUrl = `movie/top_rated?page=${currentPage}`;
     try {
-        const res = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&page=${currentPage}`);
-        
-        if(res.status !== 200) {
-            console.error(res.status)
-        } else {
-            const data = await res.json();
-            console.log(data)
+        const { data } = await api(trendingUrl);
+        console.log(data)
 
-            let movies = "";
-            data.results.forEach(movie => {
-                movies += `
-                    <article class="movie-card">
-                        <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" />
-                        <div class="description" id="description">
-                            <p class="year">${movie.release_date.slice(0,4)}</p>
-                            <h3 class="movie-title">${movie.title}</h3>
-                            <p class="average">${movie.vote_average}</p>
-                        </div>
-                    </article>
-                `;
-            });
-            document.getElementById("movies-grid-container").innerHTML = movies;
-        }
+        let movies = "";
+        data.results.forEach(movie => {
+            movies += `
+                <article class="movie-card">
+                    <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" />
+                    <div class="description" id="description">
+                        <p class="year">${movie.release_date.slice(0,4)}</p>
+                        <h3 class="movie-title">${movie.title}</h3>
+                        <p class="average">${movie.vote_average.toFixed(1)}</p>
+                    </div>
+                </article>
+            `;
+        });
+        document.getElementById("movies-grid-container").innerHTML = movies;
+        
 
     } catch {
         console.error("No se completo el fetch")
@@ -48,3 +55,24 @@ const loadMovies = async() => {
 }
 
 loadMovies();
+
+const loadGenre = async() => {
+    const categoriesUrl = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`;
+
+    try {
+        const res = await fetch(categoriesUrl)
+
+        if(res.status !== 200) {
+            console.error(res.status, res.statusText)
+        } else {
+            const data = await res.json();
+            console.log(data)
+
+            data.genres.forEach(genre => {
+                console.log(genre.id, genre.name)
+            });
+        }
+    } catch {
+        console.error("No se completo el fetch")
+    }
+}
