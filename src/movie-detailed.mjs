b29dee0deb737b404717c4296e7f9d6975efe9a8
Jesús Álvarez <khisus19@gmail.com>
2022-07-19 22:56:22 -0500

    Genre added to detailed view

--- src/movie-detailed.mjs
+++ src/movie-detailed.mjs
@@ -9,20 +9,23 @@ const movieDetailedView = async(movie_id) => {
     const res = await fetch(`${idSearchUrl}?api_key=${API_KEY}`);
     const data = await res.json();
     
+    let genre = data.genres.map(ele => ele.name).join(", ")
     details += `
     <div id="modal-card-${data.id}" class="movie-container">
         <img src="https://image.tmdb.org/t/p/w500/${data.poster_path}" />
         <div class="description-container" id="description">
             <p class="year">${data.release_date.slice(0,4)}</p>
+            <p class="genre">${genre}</p>
             <h2 class="movie-title">${data.title}</h2>
             <p class="average">${data.vote_average.toFixed(1)}</p>
+            <p class="tagline"><em>"${data.tagline}"</em></p>
             <p class="synopsis"><span>Synopsis: </span>${data.overview}</p>
             <p class="runtime"><span>Runtime: </span>${data.runtime} min.</p>
-            <p class="tagline"><span>Tagline: </span><em>"${data.tagline}"</em></p>
         </div>
     </div>
     `
     console.log(data)
+    console.log(genre)
     document.getElementById("movie-view").innerHTML = details;
 }
 console.log("id:", movieId)
